a28412ad0afdafc389e8e479e9c4feebbbf5143d
Dirk Eisenberg <dirk.eisenberg@gmail.com>
2023-04-04 08:05:28 +0200

    Asp.NET support

---
+++ CoreHelpers.Branding.AspNet/MiddlwareBrandingInjector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CoreHelpers.Branding.Runtime;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreHelpers.Branding.AspNet
+{
+    public static class MiddlwareBrandingInjector
+    {        
+        public static IApplicationBuilder UseBrandingWithRequestHost(this IApplicationBuilder app, string applicationId, Func<IBrandingManager, Task<ICompanyBranding>> defaultBrandingBuilder)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                // get the branding manager
+                var brandingManager = ctx.RequestServices.GetService<IBrandingManager>();
+                var brandingStateService = ctx.RequestServices.GetService<IBrandingStateService>();
+                if (brandingManager == null || brandingStateService == null)
+                    throw new Exception("Failed to load the branding");
+                
+                // get the current brand
+                var currentBranding = await brandingManager.FindCompanyBrandingByApplicationAndUniqueIdentifier(applicationId, ctx.Request.Host.Host.ToString().ToSha1(), null);
+                if (currentBranding == null)
+                {
+                    var defaultBranding = await defaultBrandingBuilder(brandingManager);
+                    currentBranding = await brandingManager.FindCompanyBrandingByApplicationAndUniqueIdentifier(applicationId, ctx.Request.Host.Host.ToString().ToSha1(), defaultBranding);
+                }
+
+                // set the state
+                brandingStateService.SetCurrentCompanyBranding(currentBranding);
+
+                // next middleware 
+                await next(ctx);
+            });
+
+            return app;
+        }
+    }
+}
+
---
+++ CoreHelpers.Branding.AspNet/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CoreHelpers.Branding.AspNet
+{
+    public static class StringExtension
+    {
+        public static string ToSha1(this string input)
+        {
+            using (var sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
+
